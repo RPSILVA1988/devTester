d519f54b4888577cd56d42a5b92815423e8f99ff
pinheiro1988 <comraniele@hotmail.com>
2021-10-02 19:35:39 -0300

    Navbar Component

--- zapLink/frontend/src/views/Dashboard.vue
+++ zapLink/frontend/src/views/Dashboard.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="dashboard">
     <div class="container">
-      <h1>Seu gerenciador digital de contatos</h1>
+      <h4 class="title is-4">Seu gerenciador digital de contatos</h4>
 
       <div class="columns">
         <div
-          class="column is-4" v-for="contact in contactList" :key="contact.id">
+          class="column is-4"
+          v-for="contact in contactList"
+          :key="contact.id"
+        >
           <div class="card">
             <div class="card-content">
               <div class="media">
                 <div class="media-left">
                   <figure class="image is-48x48">
-                    <img
-                      src="https://bulma.io/images/placeholders/96x96.png"
-                      alt="Placeholder image"
-                    />
+                    <img src="../assets/whatsapp.svg" alt="Logo WhatsApp" />
                   </figure>
                 </div>
 
                 <div class="media-content">
-                  <p class="title is-4">{{contact.name}}</p>
-                  <p class="subtitle is-6">{{contact.number}}</p>
+                  <p class="title is-4">{{ contact.name }}</p>
+                  <p class="subtitle is-6">{{ contact.number }}</p>
                 </div>
               </div>
 
               <div class="content">
-                {{contact.description}}
+                {{ contact.description }}
               </div>
             </div>
             <footer class="card-footer">
