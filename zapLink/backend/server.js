a34cd45af5775605752664e859d8e1eb09c305a2
pinheiro1988 <comraniele@hotmail.com>
2021-10-02 23:10:51 -0300

    Testes Unitários com Hapi/Lab

--- zapLink/backend/server.js
+++ zapLink/backend/server.js
@@ -11,40 +11,39 @@ mongoose.connection.on('connected', () => {
     console.log('MongoDB Conneted');
 })
 
-mongoose.connection.on('error', (erro) => {
+mongoose.connection.on('error', (error) => {
     console.log('MongoDB Error ' + error);
 })
 
-const contactRoutes = require('./routes/contact.routes');
+const contactRoutes = require('./routes/contact.routes')
 
-const contacyRoutes = require('./routes/contact.routes')
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
 
-const init = async () => {
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return { message: 'Welcome to ZapLink API', company: 'QA Ninja', course: 'DevTester' };
+    }
+});
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+server.route(contactRoutes)
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return { message: 'Welcome to ZapLink API', company: 'QA Ninja', course: 'DevTester' }
-        }
-    })
-
-    server.route(contactRoutes)
-
-    await server.start();
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
     console.log('Server running on %s', server.info.uri);
-};
+});
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err);
     process.exit(1);
 });
 
-init();
+exports.init = async () => {
+    return server;
+}
