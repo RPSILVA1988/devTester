10beab23357795dbe4dc2932d95bfdadd52e1385
pinheiro1988 <comraniele@hotmail.com>
2021-10-10 19:18:36 -0300

    zapLink - Estrutura model, controller e route para users

---
+++ zapLink/backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    async create(request, h) {
+        return null;
+    },
+    async login() {
+        return null;
+    }
+}
---
+++ zapLink/backend/routes/user.routes.js
@@ -0,0 +1,15 @@
+
+const UserController = require('../controllers/user.controller')
+
+module.exports = [
+    {
+        method: 'POST',
+        path: '/user',
+        handler: UserController.create
+    },
+    {
+        method: 'POST',
+        path: '/session',
+        handler: UserController.create
+    }
+]
